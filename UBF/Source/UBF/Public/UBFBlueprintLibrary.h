2051e88a6c0390ed681b6a5140c89502067d179d
Mike Lee <mike.lee@futureverse.com>
2025-04-03 13:15:32 +1300

    fix: compiler error

--- UBF/Source/UBF/Public/UBFBlueprintLibrary.h
+++ UBF/Source/UBF/Public/UBFBlueprintLibrary.h
@@ -16,6 +16,6 @@ class UBF_API UUBFBlueprintLibrary : public UBlueprintFunctionLibrary
 	GENERATED_BODY()
 
 public:
-	UFUNCTION(BlueprintCallable, meta=(WorldContext="WorldContext"))
+	UFUNCTION(BlueprintCallable)
 	static UUBFBindingObject* CreateNewInputBindingObject(const FString& Id, const FString& Type, const FString& Value);
 };
